6d25b3ad683d8e44ec57b74ec9e99ee3c5dd416f
DReyes999 <DReyes999@gmail.com>
2019-05-23 16:29:20 -0700

    Initial Commit

---
+++ TripleX.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a server room\n";
+    std::cout << "You need to enter the correct codes to continue...";
+    std::cout << std::endl; // Another Way to add a line
+
+    
+
+ 
+    return 0;
+}
